8a669b6d995af48a96291465365a383c25305846
M Joshua Masilela <34127591+JoshuaMasilela@users.noreply.github.com>
2022-03-24 03:24:54 +0200

    update: added widgets static titles

--- src/components/StaticData.js
+++ src/components/StaticData.js
@@ -4,3 +4,13 @@ import Logo from '../assets/svg/credit.svg'
 export const navObj = {
     logo: Logo
 };
+
+///small widget object 
+export const smlWidgetObj = {
+    title: "View Credit Cards"
+};
+
+///large widget object 
+export const lrgWidgetObj = {
+    titke: "Add new Credit Card"
+};
