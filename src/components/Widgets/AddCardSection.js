8a881fb1e7bb7242d9f35f111b0c279e4d97d6b0
M Joshua Masilela <34127591+JoshuaMasilela@users.noreply.github.com>
2022-03-24 07:44:39 +0200

    update: add custom hooks for use

--- src/components/Widgets/AddCardSection.js
+++ src/components/Widgets/AddCardSection.js
@@ -4,6 +4,9 @@ import { AddCardContainer, AddCardTitle } from './Elements/AddCardElements'
 export default function AddCardSection({
   title
 }) {
+
+  //custom hooks
+  const { handleChange, handleFocus, handlSubmit, values, errors } = useForms();
   return (
     <AddCardContainer>
       <AddCardTitle>{title}</AddCardTitle>
