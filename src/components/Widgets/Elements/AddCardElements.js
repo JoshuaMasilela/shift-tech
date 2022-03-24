41c74723e2a276cb4b040cb3738582ad07f43a4e
M Joshua Masilela <34127591+JoshuaMasilela@users.noreply.github.com>
2022-03-24 10:56:41 +0200

    update: widget styles

--- src/components/Widgets/Elements/AddCardElements.js
+++ src/components/Widgets/Elements/AddCardElements.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 export const AddCardContainer = styled.div`;
 flex:2;
 -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
@@ -11,4 +12,26 @@ export const AddCardTitle = styled.h3`;
 font-size: 22px;
 font-weight: 600
 text-decoration: underline;
+`;
+
+export const CardContainer = styled.div`
+margin: 15px;
+width: 80%;
+display:block;
+align-items: center;
+justify-content:: center
+`;
+
+export const Form = styled(Box)`
+margin-top: 15px;
+
+`;
+export const FormContent = styled.form`
+display: flex;
+`;
+
+export const FormInput = styled.input`
+width: 80%;
+height: 30px;
+margin: 10px;
 `;
