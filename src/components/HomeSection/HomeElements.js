2150b6177bf220a73b05b6c52021457ddd484e7f
M Joshua Masilela <34127591+JoshuaMasilela@users.noreply.github.com>
2022-03-24 03:25:12 +0200

    update: home styling

---
+++ src/components/HomeSection/HomeElements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+flex:4;
+margin-top:20px;
+`;
+
+export const WidgetContainer = styled.div`
+display:flex;
+margin: 20px;
+`;
