6134d9d129a365c08d090dfb717d43dc76c4b6cd
M Joshua Masilela <34127591+JoshuaMasilela@users.noreply.github.com>
2022-03-24 03:48:32 +0200

    feat: widget display

--- src/components/Widgets/Elements/ViewCardsElements.js
+++ src/components/Widgets/Elements/ViewCardsElements.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ViewCardsContainer = styled.div`;
-flex:1;
+flex:3;
 -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
 box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
 padding: 20px;
