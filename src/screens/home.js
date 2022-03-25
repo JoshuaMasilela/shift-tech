885a33b01ff389bf28ad5fb65d7803c9c4a3725a
M Joshua Masilela <34127591+JoshuaMasilela@users.noreply.github.com>
2022-03-25 11:30:57 +0200

    update: home screen layout

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import HomeSection from '../components/HomeSection'
+import Navbar from '../components/NavBar/Navbar'
+import { navObj } from '../components/ObjData'
 
 export default function HomeScreen() {
   return (
     <>
+    <Navbar {...navObj}/>
     <HomeSection/>
     </>
   )
