f32872ccb819c272710a3ecd8bbc7337fabd4655
M Joshua Masilela <34127591+JoshuaMasilela@users.noreply.github.com>
2022-03-25 11:29:01 +0200

    update: add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import Navbar from "./components/NavBar/Navbar";
 import { navObj } from "./components/ObjData";
 import HomeScreen from "./screens/home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ViewEditScreen from "./screens/edit";
 
 
 function App() {
   return (
-    <>
-    <Navbar {...navObj}/>
-       <HomeScreen/>
-    </>
- 
+ <Router>
+   <Routes>
+     <Route
+     path="/"
+     exact
+     element={<HomeScreen/>}/>
+
+     <Route
+     path="/edit_user/:id"
+     exact
+     element={<ViewEditScreen/>}/>
+   </Routes>
+ </Router>
   );
 }
 
