25e98e80fee4fa8fd9ede7a4b47cb84ca9cad17f
M Joshua Masilela <34127591+JoshuaMasilela@users.noreply.github.com>
2022-03-25 11:29:58 +0200

    update: table styling

--- src/components/Widgets/Elements/ViewCardsElements.js
+++ src/components/Widgets/Elements/ViewCardsElements.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { Tooltip } from '@mui/material';
+import { BsExclamationTriangleFill, BsPenFill, BsTrashFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 export const ViewCardsContainer = styled.div`;
 flex:3;
@@ -47,4 +50,56 @@ export const ViewWidgetTableDataUser = styled.td`;
 display: flex;
 align-items: center;
 font-weight: 600;
+`;
+
+export const ActionsContainer =styled.div`
+display: flex;
+align-items: center;
+`;
+
+export const DeleteAction = styled(BsTrashFill)`
+font-size: 20px;
+color: red;
+margin: 2px;
+cursor: pointer;
+`;
+
+export const EditAction = styled(BsPenFill)`
+font-size: 20px;
+color: green;
+margin: 2px;
+cursor: pointer;
+`;
+
+export const DeactivateAction = styled(BsExclamationTriangleFill)`
+font-size: 20px;
+color: gold;
+margin: 2px;
+cursor: pointer;
+`;
+
+export const ActionToolTip = styled(Tooltip)`
+color: #010606;
+align-items: center;
+font-size: 15px;
+justify-content: center;
+background-color: #f9f9f9;
+`;
+
+export const IconLink = styled(Link)`
+display: flex;
+`;
+
+export const StatusContainer = styled.div`
+display: flex;
+height: 30px;
+width:100%;
+align-items: center;
+justify-content: center;
+background: ${({status}) => (status ? 'green' : 'red')}
+`;
+
+export const StatusText = styled.p`
+color: #ffffff;
+font-size: 16;
 `;
